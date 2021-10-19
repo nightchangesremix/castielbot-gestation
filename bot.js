a1b0bd44909f704e53fe301694053beed85e6414
jessicagoel <78863710+jessicagoel@users.noreply.github.com>
2021-10-19 12:54:04 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -13,7 +13,7 @@ client.on('ready', () => {
 client.on('message', (receivedMessage) => {
     if (receivedMessage.author == client.user) {
         return
-    }
+    }`
     //receivedMessage.channel.send("hello, " + receivedMessage.author.toString() + "!")
     if (receivedMessage.content.startsWith("!")) {
         processCommand(receivedMessage)
@@ -197,7 +197,7 @@ function helpCommand(arguments, receivedMessage) {
         receivedMessage.channel.send("[is hold]")
     }
    else if (arguments == "help") {
-        receivedMessage.channel.send("Hi bestie! Here are my commands:`\n!cas talk: get a random Cas quote from canon\n!cas speak: get me to say something funny\n!cas bde: measure your big dick energy\n!cas bhe: measure your big hole energy\n!cas valid: learn how valid you are, objectively\n!cas death: learn the exact date and time of your own death\n!cas hug: give me a hug\n!cas kiss: give me a kiss\n!cas hold: hold me\n!cas feel: learn how I'm feeling\n!cas lobotomy: become Naomi-coded`\nYou can also make me say anything you want by typing `'cas say,'` get my opinions by asking `'cas what do you think,'` and I respond to my own name (plus a few others)!")
+        receivedMessage.channel.send("Hi bestie! Here are my commands:`\n!cas talk: get a random Cas quote from canon\n!cas speak: get me to say something funny\n!cas bde: measure your big dick energy\n!cas bhe: measure your big hole energy\n!cas valid: learn how valid you are, objectively\n!cas death: learn the exact date and time of your own death\n!cas hug: give me a hug\n!cas kiss: give me a kiss\n!cas hold: hold me\n!cas feel: learn how I'm feeling\n!cas lobotomy: become Naomi-coded`\n`!cas poison`: take care of me\n`!cas fuck`: Exactly what it sounds like\nYou can also make me say anything you want by typing `'cas say,'` get my opinions by asking `'cas what do you think,'` and I respond to my own name (plus a few others)!")
     }
     
     else if (arguments == "birth") {
